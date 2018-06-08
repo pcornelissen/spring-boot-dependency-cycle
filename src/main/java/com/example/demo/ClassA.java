9189e6f575c57b66d524a445215fa585c38d48bd
Patrick Cornelißen <pcornelissen@orchit.de>
2018-06-08 18:50:24 +0200

    Lazy on first class, still no luck

--- src/main/java/com/example/demo/ClassA.java
+++ src/main/java/com/example/demo/ClassA.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-@Component @AllArgsConstructor @Getter
+@Component @AllArgsConstructor @Getter @Lazy
 public class ClassA {
     private ClassB b;
 }
